644248dedb4086493cdf881b0ea5e6442cd488c0
nabilgj <nabilgj.bnm@gmail.com>
2021-01-30 10:20:32 -0500

    added node-sass  version 4 dot 14 to support with react version 17

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+import "./default.scss";
+
+import Header from "./components/Header/index";
+
 const App = () => {
   return (
     <div className="App">
+      <Header />
       <h2> Hi from react ecommerce </h2>
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import "./styles.scss";
+
+const Header = (props) => {
+  return <header className="header"></header>;
+};
+
+// into
+export default Header;
